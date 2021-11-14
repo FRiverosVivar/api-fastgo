dc7b4173666a7cf274716f358f4d9d0810f24669
friveros98 <francoriverosvivar@gmail.com>
2021-11-14 20:12:41 -0300

    .

--- src/utils/mongo-db/mongo-db.js
+++ src/utils/mongo-db/mongo-db.js
@@ -3,6 +3,7 @@ console.log(process.env.FASTGO_DB_CERT)
 const URI = 'mongodb+srv://doadmin:'+process.env.FASTGO_DB_PW+'\
 @db-mongodb-nyc1-66135-7d3baa74.mongo.ondigitalocean.com/\
 admin1?authSource=admin&replicaSet=db-mongodb-nyc1-66135&tls=true&tlsCAFile='+process.env.FASTGO_DB_CERT
+console.log(URI)
 try {
     mongoose.connect(URI);
 }catch(err){
