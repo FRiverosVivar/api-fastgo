3c3c3ba5d5880650c287ca0e00e3a2961491a0d0
friveros98 <francoriverosvivar@gmail.com>
2021-11-14 20:33:33 -0300

    .

--- src/utils/mongo-db/mongo-db.js
+++ src/utils/mongo-db/mongo-db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 console.log(process.env.FASTGO_DB_CERT)
-const URI = 'mongodb+srv://doadmin:'+process.env.FASTGO_DB_PW+'\
-@db-mongodb-nyc1-66135-7d3baa74.mongo.ondigitalocean.com/\
-admin1?authSource=admin&replicaSet=db-mongodb-nyc1-66135&tls=true&tlsCAFile='+process.env.FASTGO_DB_CERT
+// const URI = 'mongodb+srv://doadmin:'+process.env.FASTGO_DB_PW+'\
+// @db-mongodb-nyc1-66135-7d3baa74.mongo.ondigitalocean.com/\
+// admin1?authSource=admin&replicaSet=db-mongodb-nyc1-66135&tls=true&tlsCAFile='+process.env.FASTGO_DB_CERT
+const URI = 'mongodb+srv://doadmin:'+process.env.FASTGO_DB_PW+'@serverlessinstance0.bcvbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 console.log(URI)
 try {
     mongoose.connect(URI);
